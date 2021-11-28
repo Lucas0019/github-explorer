e2f6feb3f46e9d898b4b1b6c17c3d61992bb4e33
lucas <lxavier.00@outlook.com>
2021-11-27 21:14:49 -0300

    style: installed and configured base styled components of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+
 // eslint-disable-next-line
 import RoutesToManage from './routes';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <RoutesToManage />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <RoutesToManage />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
---
+++ src/pages/Dashboard/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Title = styled.h1`
+  font-size: 1.5em;
+  color: tomato;
+`;
