a9d43a8649a5fdc054fb08950bd52ec91211ce5c
lucas <lxavier.00@outlook.com>
2021-11-27 23:45:30 -0300

    feat: Created global style, add font and project img

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/background-github.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -8,10 +10,29 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    --font-family: 'Poppins', sans-serif;
+    --background-color: #F0F0F5
+  }
+
   body {
-    background: #F0F0F5;
+    background: var(--background-color) url(./${githubBackground}) no-repeat 70% top;
     color: #fff;
     -webkit-font-smoothing: antialiased;
   }
 
+  body, input, button {
+    font-family: var(--font-family), sans-serif;
+    font-size: 16px;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button {
+    cursor: pointer;
+  }
 `;
